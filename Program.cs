c5b166371eae44edc1f7f7d3d7b8a0bec00f1d9d
Kaline Cecília Constantino <kaline.constantino@gmail.com>
2023-09-22 16:05:16 -0300

    Classe PosicaoXadrez

--- Program.cs
+++ Program.cs
@@ -8,20 +8,11 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.colocarPeca(new Tower(tab, Cor.Magenta), new Posicao(0, 0));
-                tab.colocarPeca(new Tower(tab, Cor.Magenta), new Posicao(1, 3));
-                tab.colocarPeca(new King(tab, Cor.Magenta), new Posicao(0, 2));
+            Console.WriteLine(pos);
 
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.toPosicao());
 
              Console.ReadLine();
         }
---
+++ Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using Tabuleiros;
+
+namespace Xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        // método para converter a posição do xadrez para uma posição interna de matriz
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
