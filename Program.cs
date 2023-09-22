636a72c38e2420725676d2cbaaa8ba345a514848
Kaline Cecília Constantino <kaline.constantino@gmail.com>
2023-09-21 23:16:59 -0300

    Exceções personalizadas do Tabuleiro

--- Program.cs
+++ Program.cs
@@ -8,13 +8,20 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Tower(tab, Cor.Magenta), new Posicao(0, 0));
-            tab.colocarPeca(new Tower(tab, Cor.Magenta), new Posicao(1, 3));
-            tab.colocarPeca(new King(tab, Cor.Magenta), new Posicao(2, 4));
+                tab.colocarPeca(new Tower(tab, Cor.Magenta), new Posicao(0, 0));
+                tab.colocarPeca(new Tower(tab, Cor.Magenta), new Posicao(1, 3));
+                tab.colocarPeca(new King(tab, Cor.Magenta), new Posicao(0, 2));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
              Console.ReadLine();
         }
